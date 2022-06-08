5a221b548dbc42833d96828cf5309738006c3e4c
dimavrok <d.mavrokapnidis@ucl.ac.uk>
2022-06-08 21:53:53 +0100

    dimi

--- ifctolbd.py
+++ ifctolbd.py
@@ -1,17 +1,63 @@
 # %% Import packages
 
 import ifcopenshell
+import rdflib
+import pyshacl
 
 from rdflib.namespace import NamespaceManager
 from rdflib import Graph, RDF, URIRef, Literal
 
-#%% Create a namespace
-NS_om = "http://openmetrics.eu/openmetrics#"
-NS_bot = "https://w3id.org/bot#"
-NS_rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-NS_owl = "http://www.w3.org/2002/07/owl#"
-NS_schema = "http://schema.org#"
-NS_beo = "https://w3id.org/beo#"
+from pyshacl import validate
+
+#%% Parse the data from the IFC to LBD converter
+
+graph = Graph()
+graph.parse('duplex.ttl', format='ttl')
+
+shapes = Graph()
+shapes.parse('shapes.ttl', format='ttl')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dictionary for storing the datapoint ids (Building Management System)
+P = {'spaces_ids':[]}
+
+# dictionary for storing the output 
+R = {}
+#input
+
+# Query the graph to get the 3D vectors for lights and spaces
+qres = g.query("""
+    PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+    PREFIX bot: <https://w3id.org/bot#> 
+    PREFIX brick: <https://brickschema.org/schema/Brick#> 
+    PREFIX om: <http://openmetrics.eu/openmetrics#> 
+    SELECT DISTINCT ?sp ?sp_guid WHERE {
+        ?sp a bot:space . 
+        ?sp bot:hasguid ?sp_id .
+        }""")
+
+# Store results of the query in a dictionary
+for r in qres:
+	P["lights"].append({str(r.li.toPython()):r.bbli.toPython()})
+
+for r in qres:
+    P["spaces"].append({str(r.sp.toPython()):r.bbsp.toPython()})
+
+
+
 
 
 #%% Import IFC file
@@ -23,5 +69,3 @@ spaces = f.by_type("IfcSpace")
 doors = f.by_type("Ifcdoor")
 walls = f.by_type("Ifcwall")
 
-print("hi")
-# %%
