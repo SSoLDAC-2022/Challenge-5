214f3ae5667d6a40a2b013ce85d753cd6d29ee43
dimavrok <d.mavrokapnidis@ucl.ac.uk>
2022-06-13 10:02:42 +0100

    Final

--- ifctolbd.py
+++ ifctolbd.py
@@ -1,71 +1,72 @@
-# %% Import packages
-
+#%% Import packages
 import ifcopenshell
-import rdflib
-import pyshacl
-
+import json
 from rdflib.namespace import NamespaceManager
-from rdflib import Graph, RDF, URIRef, Literal
+from rdflib import Graph, RDF, URIRef, Literal, RDF
 
-from pyshacl import validate
-
-#%% Parse the data from the IFC to LBD converter
-
-graph = Graph()
-graph.parse('duplex.ttl', format='ttl')
-
-shapes = Graph()
-shapes.parse('shapes.ttl', format='ttl')
-
-
-
-
-
-
-
-
-
-
-
-
-
-# dictionary for storing the datapoint ids (Building Management System)
-P = {'spaces_ids':[]}
-
-# dictionary for storing the output 
-R = {}
-#input
-
-# Query the graph to get the 3D vectors for lights and spaces
-qres = g.query("""
-    PREFIX owl: <http://www.w3.org/2002/07/owl#> 
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-    PREFIX bot: <https://w3id.org/bot#> 
-    PREFIX brick: <https://brickschema.org/schema/Brick#> 
-    PREFIX om: <http://openmetrics.eu/openmetrics#> 
-    SELECT DISTINCT ?sp ?sp_guid WHERE {
-        ?sp a bot:space . 
-        ?sp bot:hasguid ?sp_id .
-        }""")
-
-# Store results of the query in a dictionary
-for r in qres:
-	P["lights"].append({str(r.li.toPython()):r.bbli.toPython()})
-
-for r in qres:
-    P["spaces"].append({str(r.sp.toPython()):r.bbsp.toPython()})
-
-
-
-
+#%% Create a namespace
+NS_om = "http://openmetrics.eu/openmetrics#"
+NS_beo = "https://pi.pauwel.be/voc/buildingelement#"
+NS_rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+NS_rdfs = "http://www.w3.org/2000/01/rdf-schema#"
+NS_owl = "http://www.w3.org/2002/07/owl#"
+NS_schema = "http://schema.org#"
+NS_mat = "http://bimerr.iot.linkeddata.es/def/material-properties#"
+NS_props = "https://w3id.org/props#"
 
 #%% Import IFC file
-f = ifcopenshell.open("duplex.ifc")
 
-buildings = f.by_type("IfcBuilding")
-stories = f.by_type("IfcBuildingStorey")
-spaces = f.by_type("IfcSpace")
-doors = f.by_type("Ifcdoor")
-walls = f.by_type("Ifcwall")
+f = ifcopenshell.open("Atlas_8_floor.ifc")
+#f = ifcopenshell.open("101.ifc")
 
+walls = f.by_type("IfcWall")
+
+
+#%% 
+graph = Graph()
+graph.namespace_manager.bind("om", URIRef(NS_om))
+graph.namespace_manager.bind("props", URIRef(NS_props))
+graph.namespace_manager.bind("mat", URIRef(NS_mat))
+graph.namespace_manager.bind("rdf", URIRef(NS_rdf))
+graph.namespace_manager.bind("owl", URIRef(NS_owl))
+graph.namespace_manager.bind("schema", URIRef(NS_schema))
+graph.namespace_manager.bind("beo", URIRef(NS_beo))
+
+
+#%% Converter 
+for wall in walls: 
+        inst_wall = URIRef(NS_om + "inst_wall_" + wall.GlobalId.replace("$","_")[16:])
+        beo_wall = URIRef(NS_beo + "wall")
+        graph.add((inst_wall, RDF.type, beo_wall))
+        try:
+                material_layers = wall.HasAssociations[0].RelatingMaterial.ForLayerSet.MaterialLayers
+                for m_l in material_layers:
+                        # relationship wall hasmateriallayer
+                        inst_material_layer = URIRef(NS_om + "inst_material_layer_" + wall.GlobalId.replace("$","_")[16:])
+                        hasMaterialLayer = URIRef(NS_mat + "hasMaterialLayer")
+                        mat_material_layer = URIRef(NS_mat + "MaterialLayer")
+                        graph.add((inst_material_layer, RDF.type, mat_material_layer))
+                        graph.add((inst_wall, hasMaterialLayer, inst_material_layer))
+                        #relations material_layer props:thickness, props:name
+                        props_layerName = URIRef(NS_props + "hasName")
+                        props_layerThickness = URIRef(NS_props + "hasThickness")
+                        props_layerDensity = URIRef(NS_props + "hasDensity")
+                        graph.add((inst_material_layer, props_layerThickness, Literal(str(m_l.LayerThickness))))                       
+                        graph.add((inst_material_layer, props_layerName, Literal(str(m_l.Name))))
+                        # relation --> materialLayer mat:hasMaterial mat:material
+                        hasMaterial = URIRef(NS_mat + "hasMaterial")                        
+                        inst_material = URIRef(NS_om + "inst_material_" + wall.GlobalId.replace("$","_")[16:])
+                        mat_material = URIRef(NS_mat + "Material")
+                        graph.add((inst_material, RDF.type, mat_material))
+                        graph.add((inst_material_layer, hasMaterial, inst_material))
+                        # relation --> material props:hasDensity "float"
+                        props_hasDensity = URIRef(NS_props + "hasDensity")
+                        graph.add((inst_material, props_hasDensity, Literal(str(m_l.Material.Density))))
+        except:
+                pass 
+
+#%% Export the graph
+graph.serialize(destination="Data_Graph.ttl", format="turtle")
+
+
+# %%
